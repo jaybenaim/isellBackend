9b0dd7c01584580af2512d642ec44e7f132f0972
jaybenaim <benaimjacob@gmail.com>
2020-01-12 00:29:28 -0500

    removed clutter

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const stripe = require("stripe")("sk_test_59y42s9amXyOuAPudcbNBta500g0JElmda");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
@@ -15,27 +20,10 @@ app.get("/api/", async (req, res) => {
   res.send("API HOME");
 });
 
-var whitelist = [
-  "https://jaybenaim.github.io",
-  "http://localhost:3000",
-  "http://localhost:3000"
-];
-
-var corsOptions = {
-  origin: true,
-  methods: ["GET", "POST", "HEAD", "OPTIONS", "DELETE"],
-  credentials: true
-};
 app.use(cors());
-app.post("/api/charge", cors(), async (req, res, next) => {
+app.post("/api/charge", cors(), async (req, res) => {
   try {
-    // const headers = JSON.parse(req.headers);
-    // const { token, total } = headers;
-    // const data = JSON.parse(req.body);
-    // const data = JSON.parse(req.headers.data);
     let { total, token } = req.headers;
-    console.log(req.body);
-    // let { token, subTotal } = data;
     total = Math.floor(total * 100);
     let { status } = await stripe.charges.create({
       amount: total,
