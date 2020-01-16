40e7172fd100673ffb6f022fd093f33921257811
jaybenaim <benaimjacob@gmail.com>
2020-01-16 15:41:33 -0500

    created profile schema

---
+++ Models/Profile.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const provenceArr = [
+  "AB",
+  "BC",
+  "MB",
+  "NB",
+  "NL",
+  "NS",
+  "ON",
+  "PE",
+  "QC",
+  "SK",
+  "NT",
+  "NU",
+  "YT"
+];
+const ProfileSchema = new Schema({
+  createdAt: { type: Date, default: Date.now },
+  username: String,
+  shippingInfo: {
+    name: { type: String, required: false },
+    street: { type: String, required: false },
+    city: { type: String, required: false },
+    provence: { type: String, required: false, enum: provenceArr },
+    postalCode: {
+      type: String,
+      required: false
+    }
+  },
+
+  user: {
+    id: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  }
+});
+module.exports = mongoose.model("Profile", ProfileSchema);
