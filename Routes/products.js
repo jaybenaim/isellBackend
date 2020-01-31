a3277be25833bea2f9d38d85b85ac00d5ba8f026
jaybenaim <benaimjacob@gmail.com>
2020-01-31 13:13:31 -0500

    added patch for products/

--- Routes/products.js
+++ Routes/products.js
@@ -5,23 +5,37 @@ const mongoose = require("mongoose");
 const Product = require("../Models/Product");
 
 router.get("/", (req, res) => {
-  Product.find((err, product) => {
-    if (err) return res.status(500).send(err);
-    return res.status(200).send(product);
-  });
+  Product.find()
+    .select("-__v")
+    .exec((err, product) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).send(product);
+    });
 });
+
 router.post("/", (req, res) => {
   Product.findOrCreate(req.body, (err, product) => {
     if (err) return res.status(500).send(err);
     return res.status(200).send(product);
   });
 });
+
 router.get("/:id", (req, res) => {
   Product.findOne({ _id: req.params.id }, (err, product) => {
     if (err) return res.status(500).send(err);
     return res.status(200).send(product);
   });
 });
+
+router.patch("/:id", (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .select("-__v")
+    .exec((err, updatedProduct) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).send(updatedProduct);
+    });
+});
+
 router.delete("/:id", (req, res) => {
   Product.findByIdAndRemove(req.params.id, (err, product) => {
     if (err) return res.status(500).send(err);
