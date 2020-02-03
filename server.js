d79789a8fb147ef8ba2210114891adbbcc455f11
jaybenaim <benaimjacob@gmail.com>
2020-02-03 16:02:52 -0500

    failing imports

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const withAuth = require("./Middleware/auth");
 // const profiles = require("./routes/profiles");
-const users = require("./routes/users");
-const stripe = require("./routes/stripe");
-const products = require("./routes/products");
-const carts = require("./routes/carts");
+// const users = require("./routes/users");
+// const stripe = require("./routes/stripe");
+// const products = require("./routes/products");
+// const carts = require("./routes/carts");
 const Cart = require("./models/Cart");
 
 app.use(
