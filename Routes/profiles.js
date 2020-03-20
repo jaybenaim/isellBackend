c0aacdcc8205cfb20d207b9767e5e1994b5e7cae
jaybenaim <benaimjacob@gmail.com>
2020-03-20 10:56:24 -0400

    refactored profile routes to use es6 for return statements

--- Routes/profiles.js
+++ Routes/profiles.js
@@ -5,27 +5,17 @@ const ShippingInfo = require("../Models/ShippingInfo");
 
 router.get("/", (req, res) => {
   Profile.find((err, profile) => {
-    if (err) return res.status(500).send(err);
-    return res.status(200).send(profile);
+    return err ? res.status(500).send(err) : res.status(200).send(profile);
   });
 });
 
 router.post("/", (req, res) => {
-  Profile.findOrCreate({ username: req.body.username }, (err, profile) => {
-    ShippingInfo.findOrCreate(
-      { street: req.body.shippingInfo.street },
-      (err, info) => {
-        if (err) {
-          return res.status(500).send(err);
-        } else {
-          const results = {
-            profile,
-            status: "Successfully created profile."
-          };
-          return res.status(200).send(results);
-        }
-      }
-    );
+  Profile.findOrCreate(req.body, (err, profile) => {
+    const results = {
+      profile,
+      status: "Successfully created profile."
+    };
+    return err ? res.status(500).send(err) : res.status(200).send(results);
   });
 });
 // find cart from user id
@@ -34,17 +24,12 @@ router.get("/find/:id", (req, res) => {
     .populate("users")
     .select("-__v")
     .exec((err, cart) => {
-      if (err) {
-        return res.status(500).send(err);
-      } else {
-        return res.status(200).send(cart);
-      }
+      return err ? res.status(500).send(err) : res.status(200).send(cart);
     });
 });
 router.get("/:id", (req, res) => {
   Profile.findOne({ _id: req.params.id }, (err, profile) => {
-    if (err) return res.status(500).send(err);
-    return res.status(200).send(profile);
+    return err ? res.status(500).send(err) : res.status(200).send(profile);
   });
 });
 
@@ -54,8 +39,6 @@ router.patch("/:id", (req, res) => {
     { _id: req.params.id },
     { new: true },
     (err, profile) => {
-      // this way list needs to be altered and passed through front-end
-
       const addresses = shippingInfo.map(i => new ShippingInfo(i));
       profile.shippingInfo = [...addresses];
       profile.save();
@@ -70,11 +53,10 @@ router.patch("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   Profile.findByIdAndRemove(req.params.id, (err, profile) => {
-    if (err) return res.status(500).send(err);
     const response = {
       message: "Profile deleted successfully"
     };
-    return res.status(200).send(response);
+    return err ? res.status(500).send(err) : res.status(200).send(response);
   });
 });
 
