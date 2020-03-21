72f27d8f8335c16a2d6c8998b574af8205576186
jaybenaim <benaimjacob@gmail.com>
2020-03-21 15:05:35 -0400

    added upsert to find/:id

--- Routes/profiles.js
+++ Routes/profiles.js
@@ -23,7 +23,8 @@ router.post("/", (req, res) => {
 router.get("/find/:id", (req, res) => {
   Profile.findByIdAndUpdate(
     { "user.id": req.params.id },
-    { user: { _id: req.params.id } }
+    { user: { id: req.params.id } },
+    { upsert: true }
   )
     .populate("shippingInfo")
     .exec((err, profile) => {
@@ -39,6 +40,7 @@ router.get("/:id", (req, res) => {
 
 router.patch("/:id", (req, res) => {
   const { shippingInfo } = req.body;
+
   Profile.findByIdAndUpdate(req.params.id, req.body, {
     new: true
   }).exec((err, profile) => {
